33e070eef391ecc2264193905ee5027029450875
Guilherme Alberto de Moraes <g.alberto.moraes@gmail.com>
2021-09-20 17:11:25 -0300

    Refatorar para melhor legibilidade

--- src/App.java
+++ src/App.java
@@ -1,14 +1,12 @@
+import static java.time.temporal.ChronoUnit.HOURS;
+
 import java.time.LocalTime;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalUnit;
-import java.util.List;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class App {
 
-    private static final TemporalUnit TIME = ChronoUnit.HOURS;
     private static final int TOTAL = 8;
 
     public static void main(String[] args) {
@@ -18,8 +16,8 @@ public class App {
         var startPause = times.get(1);
         var endPause = times.get(2);
 
-        var firstShift = TIME.between(startWorking, startPause);
-        var endWork = endPause.plus(TOTAL, TIME).minus(firstShift, TIME);
+        var firstShift = HOURS.between(startWorking, startPause);
+        var endWork = endPause.plus(TOTAL, HOURS).minus(firstShift, HOURS);
 
         var message = String.format("Seu expediente termina às %s", endWork);
         Logger.getGlobal().info(message);
